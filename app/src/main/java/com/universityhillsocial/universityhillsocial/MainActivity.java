2fbe1e0b2449f31a9cb7f976b0421c7e3aa63c4d
Kubie <yanicakj@gmail.com>
2018-02-19 00:32:17 -0500

    First Intent added

--- app/src/main/java/com/universityhillsocial/universityhillsocial/MainActivity.java
+++ app/src/main/java/com/universityhillsocial/universityhillsocial/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 import android.app.Activity;
 import android.view.Menu;
 import android.app.IntentService;
@@ -17,13 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
         Button LogInButton = (Button) findViewById(R.id.LogInButton);
 
-        View.OnClickListener listener = new View.OnClickListener() {
+        LogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changeScreen = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(changeScreen);
+            }
+        });
+
+        /*View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                //Intent changeScreen = new Intent()
+                Intent changeScreen = new Intent(MainActivity.this, WelcomeActivity.class);
+                MainActivity.this.startActivity(changeScreen);
             }
-        };
+        };*/
 
     }
 }
