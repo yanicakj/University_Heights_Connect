76136d2e50cd61b94cda0dc7c6f5d34f44e9300c
Kubie <yanicakj@gmail.com>
2018-03-07 23:08:34 -0500

    Added input validation & setViews function

--- app/src/main/java/com/universityhillsocial/universityhillsocial/MainActivity.java
+++ app/src/main/java/com/universityhillsocial/universityhillsocial/MainActivity.java
@@ -8,25 +8,67 @@ import android.content.Intent;
 import android.app.Activity;
 import android.view.Menu;
 import android.app.IntentService;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userEmail, userPassword;
+    private Button LoginButton;
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button LogInButton = (Button) findViewById(R.id.LogInButton);
+        setViews();
 
-        LogInButton.setOnClickListener(new View.OnClickListener() {
+        LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent changeScreen = new Intent(getApplicationContext(), WelcomeActivity.class);
-                startActivity(changeScreen);
+                if (validate()) {
+                    // move to profile screen
+
+                    //Intent changeScreen = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    //startActivity(changeScreen);
+                }
             }
         });
 
-
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
 
     }
+
+    private void setViews() {
+
+        userEmail = (EditText) findViewById(R.id.userEmail);
+        userPassword = (EditText) findViewById(R.id.userPassword);
+        LoginButton = (Button) findViewById(R.id.LogInButton);
+        signUp = (TextView) findViewById(R.id.SignUp);
+
+    }
+
+    private Boolean validate() {
+
+        Boolean filled = false;
+        String inputEmail = userEmail.getText().toString();
+        String inputPassword = userPassword.getText().toString();
+
+        if (inputEmail.isEmpty() || inputPassword.isEmpty())
+            Toast.makeText(this, "Please fill out all of the fields", Toast.LENGTH_SHORT).show();
+        else
+            filled = true;
+
+        return filled;
+    }
+
+
 }
+
--- app/src/main/java/com/universityhillsocial/universityhillsocial/RegistrationActivity.java
+++ app/src/main/java/com/universityhillsocial/universityhillsocial/RegistrationActivity.java
@@ -1,15 +1,47 @@
 package com.universityhillsocial.universityhillsocial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText registerEmail, registerPassword;
+    private Button registerButton;
+    private TextView loginJumpBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        setViews();
+
+
+        loginJumpBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+            }
+        });
+
+
 
     }
+
+    private void setViews() {
+
+        registerButton = (Button) findViewById(R.id.RegisterButton);
+        registerEmail = (EditText) findViewById(R.id.registerEmail);
+        registerPassword = (EditText) findViewById(R.id.registerPassword);
+        loginJumpBack = (TextView) findViewById(R.id.LoginJumpBack);
+
+    }
+
+
 }
